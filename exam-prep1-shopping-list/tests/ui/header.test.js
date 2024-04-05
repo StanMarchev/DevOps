9d6091916fbb75fd55ec0d6264f2f61e09579aab
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2024-04-05 19:08:04 +0100

    fix tests

--- exam-prep1-shopping-list/tests/ui/header.test.js
+++ exam-prep1-shopping-list/tests/ui/header.test.js
@@ -2,8 +2,8 @@ const { test, expect } = require('@playwright/test');
 
 test('Check header', async ({ page }) => {
     await page.goto('http://localhost:8080'); 
-    const homeLink = await page.$('p[href="/"]');
+    const homeLink = await page.waitForSelector('p[href="/"]', { timeout: 5000, visible: true });
     const text = await homeLink.textContent();
-    expect(text).toBe('My Shopping List for Tomorrow');
+    expect(text).toBe('This is the Regular exam for Software Engineering and DevOps course @ SoftUni');
   });
   
